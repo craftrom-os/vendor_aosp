ca249b6041f77c4690594ecc249a22851f20bf59
Galitsky Dmytro <melles.android@gmail.com>
2024-07-01 22:53:37 +0300

    Update main_version.mk

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -13,9 +13,22 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.modversion=$(CUSTOM_VERSION) \
     org.craftrom.build_type=$(CUSTOM_BUILD_TYPE)
 
-# Updater
-ifeq ($(IS_OFFICIAL),true)
-    ADDITIONAL_SYSTEM_PROPERTIES  += \
-        net.pixelos.build_type=ci \
-        net.pixelos.version=$(CUSTOM_VERSION_PROP)
-endif
+# LineageOS System Version
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.lineage.version=$(LINEAGE_VERSION) \
+    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
+    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(LINEAGE_VERSION) \
+    ro.lineagelegal.url=https://lineageos.org/legal
+
+# LineageOS Platform Display Version
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
+
+# LineageOS Platform SDK Version
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
+
+# LineageOS Platform Internal Version
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
